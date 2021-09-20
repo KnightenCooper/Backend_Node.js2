314ed547c4030d239a3f6d17f9a44183705c25a2
KnightenCooper <coo18028@byui.edu>
2021-09-20 11:35:19 -0500

    testing heroku: uncomment code

--- routes/ta01.js
+++ routes/ta01.js
@@ -37,28 +37,28 @@ router.get('/', (req, res, next) => {
 
 
 
-// //   // Request handling
-// //   // CORE CHALLENGE 1 -
-// //   // HTML page is written
-// //   res.write('<html>');
-// //   res.write('<head><title>Hello Browser!</Title></head>');
-// //   res.write('<body>');
-// //   res.write('<h1>Welcome to my world!</h1>');
-// //   // navigation to your activities endpoint.
-// //   res.write('<a href="/">Home</a></br>');
-// //   res.write('<a href="ta01/activities">Activities List</a></br>');
-// //   // These are navigation links for the stretch challenges
-// //   res.write('<a href="ta01/stretch-1">Stretch 1 (CSS)</a></br>');
-// //   res.write(
-// //     '<a href="ta01/stretch-2">Stretch 2 (Write Form input to text input)</a></br>'
-// //   );
-// //   res.write(
-// //     '<a href="ta01/stretch-3">Stretch 3 (Add two number inputs together)</a></br>'
-// //   );
-// //   res.write('</body>');
-// //   res.write('</html>');
-// //   return res.end(); // Return so you don't execute remaining code outside of if statement
-// // });
+  // Request handling
+  // CORE CHALLENGE 1 -
+  // HTML page is written
+  res.write('<html>');
+  res.write('<head><title>Hello Browser!</Title></head>');
+  res.write('<body>');
+  res.write('<h1>Welcome to my world!</h1>');
+  // navigation to your activities endpoint.
+  res.write('<a href="/">Home</a></br>');
+  res.write('<a href="ta01/activities">Activities List</a></br>');
+  // These are navigation links for the stretch challenges
+  res.write('<a href="ta01/stretch-1">Stretch 1 (CSS)</a></br>');
+  res.write(
+    '<a href="ta01/stretch-2">Stretch 2 (Write Form input to text input)</a></br>'
+  );
+  res.write(
+    '<a href="ta01/stretch-3">Stretch 3 (Add two number inputs together)</a></br>'
+  );
+  res.write('</body>');
+  res.write('</html>');
+  return res.end(); // Return so you don't execute remaining code outside of if statement
+});
 
 
 
