5ffcea56fcb7e6959a99b90d42107dabb74a7a38
KnightenCooper <coo18028@byui.edu>
2021-09-20 14:30:06 -0500

    remove book route

---
+++ routes/books.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+
+// For requirement 01 of books
+const userArray = ['Jack', 'Jill', 'Brian'];
+
+// For requirement 02 of books
+router.post('/addUser', (req, res, next) => {
+    const newUser = req.body.newUser;
+
+    userArray.push(newUser);
+
+    res.redirect('/books/');
+});
+
+// For requirement 03 of books
+router.post('/removeUser', (req, res, next) => {
+    const remUser = req.body.remUser;
+
+    // Splice method removes from a const array
+    const index = userArray.indexOf(remUser);
+    if (index !== -1 ) {
+        userArray.splice(index, 1);
+    }
+
+    res.redirect('/books/');
+});
+
+router.get('/',(req, res, next) => {
+    res.render('pages/books', {
+        title: 'Team Activity 02',
+        users: userArray,
+        path: '/books', // For pug, EJS
+        activeTA03: true, // For HBS
+        contentCSS: true, // For HBS
+    });
+});
+
+module.exports = router;
+
