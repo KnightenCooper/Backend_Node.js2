8300fb8b8c812703d5a22bb601e8635b9e60637d
KnightenCooper <coo18028@byui.edu>
2021-09-20 17:07:28 -0500

    remove price code

--- routes/books.js
+++ routes/books.js
@@ -3,30 +3,24 @@ const router = express.Router();
 
 // For requirement 01 of books
 const bookArray = [];
-const bookArrayPrice = [];
+
 // For requirement 02 of books
 router.post('/addBook', (req, res, next) => {
-  const newBook = req.body.newBook;
-  const newBookPrice = req.body.newBookPrice;
+    const newBook = req.body.newBook;
 
     bookArray.push(newBook);
-    bookArrayPrice.push(newBookPrice);
-    
 
     res.redirect('/books/');
 });
 
 // For requirement 03 of books
 router.post('/removeBook', (req, res, next) => {
-  const remBook = req.body.remBook;
-  const remBookPrice = req.body.remBookPrice;
+    const remBook = req.body.remBook;
 
     // Splice method removes from a const array
     const index = bookArray.indexOf(remBook);
-    const index = bookArrayPrice.indexOf(remBookPrice);
     if (index !== -1 ) {
-      bookArray.splice(index, 1);
-      bookArrayPrice.splice(index, 1);
+        bookArray.splice(index, 1);
     }
 
     res.redirect('/books/');
