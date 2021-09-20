8733e6067b5f62a51168943be005988cf7ee0baa
KnightenCooper <coo18028@byui.edu>
2021-09-20 15:47:17 -0500

    change book page to say book

--- routes/books.js
+++ routes/books.js
@@ -5,20 +5,20 @@ const router = express.Router();
 const bookArray = [];
 
 // For requirement 02 of books
-router.post('/addUser', (req, res, next) => {
-    const newUser = req.body.newUser;
+router.post('/addBook', (req, res, next) => {
+    const newBook = req.body.newBook;
 
-    bookArray.push(newUser);
+    bookArray.push(newBook);
 
     res.redirect('/books/');
 });
 
 // For requirement 03 of books
-router.post('/removeUser', (req, res, next) => {
-    const remUser = req.body.remUser;
+router.post('/removeBook', (req, res, next) => {
+    const remBook = req.body.remBook;
 
     // Splice method removes from a const array
-    const index = bookArray.indexOf(remUser);
+    const index = bookArray.indexOf(remBook);
     if (index !== -1 ) {
         bookArray.splice(index, 1);
     }
@@ -28,8 +28,8 @@ router.post('/removeUser', (req, res, next) => {
 
 router.get('/',(req, res, next) => {
     res.render('pages/books', {
-        title: 'Team Activity 02',
-        users: bookArray,
+        title: 'Prove Activity 02',
+        Books: bookArray,
         path: '/books', // For pug, EJS
         activeTA03: true, // For HBS
         contentCSS: true, // For HBS
